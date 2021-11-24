f4cdb59d724b3d38b5c53f6da643003b29a90e82
Kaylee-dev <70692202+Kaylee-dev@users.noreply.github.com>
2021-11-24 14:08:19 +0100

    lab2 gemaakt

---
+++ feestLunch.py
@@ -0,0 +1,5 @@
+croissaints = 0.39
+bread = 2.78
+discount = 0.50
+
+print(croissaints * 17 + bread * 2 - discount * 3)
---
+++ speelhalDag.py
@@ -0,0 +1,5 @@
+ticket = 7.45
+gameSeat = 0.37
+minutes = 9
+
+print(ticket * 3 + gameSeat * minutes * 3)
